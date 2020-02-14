d7e3b7697793a8d44b540f58121593f02fdf9c33
Todd Spainhour <toddspainhour@gmail.com>
2020-02-13 21:52:26 -0600

    event listeners functional

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-console.log("Your JavaScript is connected.")
-
-
 
 const pets = [
     {
@@ -123,20 +120,6 @@ const pets = [
       imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
     },
     {
-      name: "Salem",
-      color: "Poop-Colored",
-      specialSkill: "Proficient in air guitar",
-      type: "dino",
-      imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
-    },
-    {
-      name: "Callie",
-      color: "Blue",
-      specialSkill: "Listens attentively to boring stories.",
-      type: "dog",
-      imageUrl: "http://dailynewsdig.com/wp-content/uploads/2014/03/Creative-And-Funny-Dog-Stock-Photography-Pictures-2.jpg"
-    },
-    {
       name: "Spooky",
       color: "Black",
       specialSkill: "Uses litter box at appropriate hours.",
@@ -151,13 +134,6 @@ const pets = [
       imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
     },
     {
-      name: "Snuggles",
-      color: "Orange",
-      specialSkill: "Is comfortable with jokes about his receding hairline.",
-      type: "cat",
-      imageUrl: "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg"
-    },
-    {
       name: "Buddy",
       color: "Red",
       specialSkill: "Enjoys fine wine.",
@@ -170,13 +146,6 @@ const pets = [
       specialSkill: "Participates in karaoke but does not force others to go out to karaoke.",
       type: "dog",
       imageUrl: "http://www.dogbreedplus.com/dog_breeds/images/basset-hound-4.jpg"
-    },
-    {
-      name: "Salem",
-      color: "Red",
-      specialSkill: "Knows the words to 4 rap songs.",
-      type: "cat",
-      imageUrl: "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg"
     },
     {
       name: "Bubba",
@@ -206,83 +175,57 @@ const pets = [
       type: "cat",
       imageUrl: "http://img.izismile.com/img/img2/20090219/cats_02.jpg"
     },
-    {
-      name: "Lucy",
-      color: "Red",
-      specialSkill: "Doesn’t get weirded out by the word “moist.”",
-      type: "dino",
-      imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
-    }
   ];
-
-
-  //the function below doesn't change. It prints to your DOM using your elementID
-
   
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
 
-
-
-const buildPetCards = () => {
+const buildPetCards = (monkeyButtArray) => {
   let domString = "";
-    for(i = 0; i < pets.length; i++) {
+    for(i = 0; i < monkeyButtArray.length; i++) {
         domString += '<div class="animals">';
-        domString += `<p class="pet-name">${pets[i].name}</p>`;
-        domString += `<img src=${pets[i].imageUrl} class="pet-image">`;
-        domString += `<p class="pet-color">${pets[i].color}</p>`;
-        domString += `<p class="pet-skill">${pets[i].specialSkill}</p>`;
-        domString += `<p class="${pets[i].type}Card">${pets[i].type}</p>`;
+        domString +=    `<p class="pet-name">${monkeyButtArray[i].name}</p>`;
+        domString +=      `<img src=${monkeyButtArray[i].imageUrl} class="pet-image">`;
+        domString +=        `<p class="pet-color">${monkeyButtArray[i].color}</p>`;
+        domString +=      `<p class="pet-skill">${monkeyButtArray[i].specialSkill}</p>`;
+        domString +=    `<p id="${monkeyButtArray[i].type}">${monkeyButtArray[i].type}</p>`;
         domString += '</div>';
     } 
     printToDom("pet-barn", domString);
 }
 
-
-
-/*
-const animalTypeButtons = (e) => {
-  const buttonId = e.target.divId //i feel like this DivId could cause issues
-  if (buttonId === "all-button") {
-    buildPetCards(pets);
+const findMyPets = (e) => {  
+  const buttonId = e.target.id; 
+    if(buttonId === 'all'){
+       buildPetCards(pets);
   } else {
-      const petTypes = []
-      for(i = 0; i < pets.length; i++) {
-        if(pets[i].type === buttonId){
-          petTypes.push(pets[i]);
-        }
+      const myPets = [];
+        for(let i = 0; i < pets.length; i++) {
+          if(pets[i].type === buttonId){
+            myPets.push(pets[i]);
       }
-        buildPetCards(petTypes, buildPetCards(pets))
   }
-}
-*/
-
-
-const catButton = () => {
-  const catsOnly = []
-  if(pets[i].type === "cat") {
-    catsOnly.push(pets[i])
+  buildPetCards(myPets);
   }
-}
-
-
+};
 
 const events = () => {
-  document.getElementById('cat').addEventListener('click', catOnly);
-  document.getElementById('dog').addEventListener('click', animalTypeButtons);
-  document.getElementById('dino').addEventListener('click', animalTypeButtons);
-  document.getElementById('all-button').addEventListener('click', animalTypeButtons)
-    }
+  document.getElementById("cat").addEventListener('click', findMyPets);
+  document.getElementById("dog").addEventListener('click', findMyPets);
+  document.getElementById("dino").addEventListener('click', findMyPets);
+  document.getElementById("all").addEventListener('click', findMyPets);
+  };
 
- 
 const init = () => {
   buildPetCards(pets)
   events()
 }
 
-init()
+init();
 
-//console.log(buildPetCards())
+/*the reason I struggled with this excercise is because 
+I was defining the argument (pets array) directly into the printToDom function 
+instead of using a parameter. This was preventing it from reprinting the new array (myPets) to the DOM */
   
