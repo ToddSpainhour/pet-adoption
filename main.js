b70a08af2ba838eb913e3175a125555bd844ab94
Todd Spainhour <toddspainhour@gmail.com>
2020-03-27 21:50:14 -0500

    cleaned up code

--- main.js
+++ main.js
@@ -2,98 +2,98 @@
 const pets = [
     {
       name: "Dusty",
-      color: "Green",
+      color: "Gray",
       specialSkill: "Gives sincere apologies.",
       type: "cat",
       imageUrl: "http://kittentoob.com/wp-content/uploads/2015/06/funny-cat-with-a-towel.jpg"
     },
     {
       name: "Trouble",
-      color: "Poop-Colored",
+      color: "Brown",
       specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
       type: "dino",
       imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg"
     },
     {
       name: "Whiskers",
-      color: "Yellow",
+      color: "Green",
       specialSkill: "Can prove he is a real man by drinking whiskey.",
       type: "dino",
       imageUrl: "https://mydinosaurs.com/wp-content/uploads/2017/02/2-3-600x400.jpg"
     },
     {
       name: "Coco",
-      color: "Black",
+      color: "Yellow",
       specialSkill: "Burps minimally.",
       type: "dog",
       imageUrl: "http://cdn.akc.org/content/article-body-image/funny-pembroke_welsh_corgi.jpg"
     },
     {
       name: "Spooky",
-      color: "Brown",
+      color: "Gray",
       specialSkill: "Comfortable in the outdoors for up to eight hours.",
       type: "cat",
       imageUrl: "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"
     },
     {
       name: "Tiger",
-      color: "Black",
+      color: "Brown",
       specialSkill: "Can read (but cannot understand) Hebrew.",
       type: "dog",
       imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Basset_Hound_600.jpg"
     },
     {
       name: "Oreo",
-      color: "Yellow",
+      color: "White",
       specialSkill: "Able to stop chewing ice or whistling on request.",
       type: "cat",
       imageUrl: "https://i.pinimg.com/originals/9d/da/3e/9dda3e5fd2b9886fc3d13ee47f52e8a0.jpg"
     },
     {
       name: "Ginger",
-      color: "Grey",
+      color: "Gray",
       specialSkill: "Comfortable in the outdoors for up to eight hours.",
       type: "dino",
       imageUrl: "http://www.theouthousers.com/images/jck//ThanosCopter/news/grumpasaur.jpg"
     },
     {
       name: "Sassy",
-      color: "Poop-Colored",
+      color: "Gray",
       specialSkill: "Adept at talking self and others out of fights.",
       type: "cat",
       imageUrl: "https://tailandfur.com/wp-content/uploads/2015/09/40-Amazing-Cat-Funny-Moment-Pictures-Feature-Image.jpg"
     },
     {
       name: "Sammy",
-      color: "Blue",
+      color: "Yellow",
       specialSkill: "Listens attentively to boring stories.",
       type: "cat",
       imageUrl: "https://i.pinimg.com/originals/04/54/92/0454926d39eeb420f4f01948e94e9e41.jpg"
     },
     {
       name: "Coco",
-      color: "Orange",
+      color: "Green",
       specialSkill: "Can be around food without staring creepily at it.",
       type: "dino",
       imageUrl: "http://www.cantref.com/userfiles/events/walking%20dino.jpg?w=600"
     },
     {
       name: "Buster",
-      color: "Green",
+      color: "White",
       specialSkill: "Does not use excessive acronyms.",
       type: "dog",
       imageUrl: "https://i.pinimg.com/originals/1f/30/8c/1f308c9c108a384b91b39430cc7312e9.jpg"
     },
     {
       name: "Chester",
-      color: "Red",
+      color: "Black",
       specialSkill: "Expertly quotes and recognizes dialogue from early seasons of The Simpsons.",
       type: "dog",
       imageUrl: "http://1kjkdg1axrkd2g03cnboj761.wpengine.netdna-cdn.com/wp-content/uploads/2017/12/braydon-anderson-105552-e1512684107659.jpg"
     },
     {
       name: "Samantha",
-      color: "Brown",
+      color: "Black",
       specialSkill: "Always up for dessert.",
       type: "cat",
       imageUrl: "http://3.bp.blogspot.com/-RzIFLsIO-XQ/UFoMLOT66JI/AAAAAAAAVps/JRF98hdA9S8/s640/funny-cat-pictures-016-027.jpg"
@@ -107,35 +107,28 @@ const pets = [
     },
     {
       name: "Smokey",
-      color: "Poop-Colored",
+      color: "Gray",
       specialSkill: "Drives at a safe rate of speed in snow or rain.",
       type: "dino",
       imageUrl: "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg"
     },
     {
       name: "Muffin",
-      color: "Yellow",
+      color: "White",
       specialSkill: "Does not freak out if you haven’t seen his favorite movie (The Big Lebowski).",
       type: "cat",
       imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
     },
     {
       name: "Spooky",
-      color: "Black",
+      color: "Gray",
       specialSkill: "Uses litter box at appropriate hours.",
       type: "cat",
       imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg"
     },
     {
-      name: "Miss kitty",
-      color: "Red",
-      specialSkill: "Owns a Nintendo Power Glove.",
-      type: "dino",
-      imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
-    },
-    {
       name: "Buddy",
-      color: "Red",
+      color: "Black",
       specialSkill: "Enjoys fine wine.",
       type: "dog",
       imageUrl: "http://1.bp.blogspot.com/-VjM0CmtN-vU/T7YX-LXa09I/AAAAAAAADA0/Vt1oGWEG0lw/s1600/sheepdog+border+collie+shakes+off+water+funny+picture+photo+pulling+faces+raspberry+tongue.jpg"
@@ -149,28 +142,28 @@ const pets = [
     },
     {
       name: "Bubba",
-      color: "Yellow",
+      color: "Gray",
       specialSkill: "Cleans himself.",
       type: "dog",
       imageUrl: "https://www.thetrendywhippet.co.uk/wp-content/uploads/2016/11/IMG_1651-600x400.jpg"
     },
     {
       name: "Chloe",
-      color: "Green",
+      color: "Brown",
       specialSkill: "Admits he is wrong",
       type: "dino",
       imageUrl: "https://assets.creationmuseum.org/img/pages/1703-DinoDen-TwoCard.jpg"
     },
     {
       name: "Nala",
-      color: "Purple",
+      color: "Gray",
       specialSkill: "Dances when he has to.",
       type: "cat",
       imageUrl: "https://tailandfur.com/wp-content/uploads/2016/03/40-Scary-and-Funny-Cat-Pictures-8.jpg"
     },
     {
       name: "Oscar",
-      color: "Green",
+      color: "Gray",
       specialSkill: "Gives hugs with appropriate pressure and for the right length of time.",
       type: "cat",
       imageUrl: "http://img.izismile.com/img/img2/20090219/cats_02.jpg"
@@ -182,15 +175,15 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
-const buildPetCards = (monkeyButtArray) => {
+const buildPetCards = (array) => {
   let domString = "";
-    for(i = 0; i < monkeyButtArray.length; i++) {
+    for(i = 0; i < array.length; i++) {
         domString += '<div class="animals">';
-        domString +=    `<p class="pet-name">${monkeyButtArray[i].name}</p>`;
-        domString +=      `<img src=${monkeyButtArray[i].imageUrl} class="pet-image">`;
-        domString +=        `<p class="pet-color">${monkeyButtArray[i].color}</p>`;
-        domString +=      `<p class="pet-skill">${monkeyButtArray[i].specialSkill}</p>`;
-        domString +=    `<p id="${monkeyButtArray[i].type}">${monkeyButtArray[i].type}</p>`;
+        domString += `<p class="pet-name">${array[i].name}</p>`;
+        domString += `<img src=${array[i].imageUrl} class="pet-image">`;
+        domString += `<p class="pet-color">${array[i].color}</p>`;
+        domString += `<p class="pet-skill">${array[i].specialSkill}</p>`;
+        domString += `<p id="${array[i].type}">${array[i].type}</p>`;
         domString += '</div>';
     } 
     printToDom("pet-barn", domString);
@@ -225,7 +218,4 @@ const init = () => {
 
 init();
 
-/*the reason I struggled with this excercise is because 
-I was defining the argument (pets array) directly into the printToDom function 
-instead of using a parameter. This was preventing it from reprinting the new array (myPets) to the DOM */
   
